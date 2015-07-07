1c0e8ba0c5c3f5e3a593c64a8dcbf20b230de782
Pim Derks <info@pimderks.com>
2015-07-07 14:16:26 +0200

    Offset particles to mouse position.

--- particle-fountain-gravity-mouse/script.js
+++ particle-fountain-gravity-mouse/script.js
@@ -4,10 +4,11 @@ var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 var particles = [];
 var colors = ['#029DAF', '#E5D599', '#FFC219', '#F07C19', '#E32551'];
-var amount = 200;
+var amount = 500;
 var speedX = 1;
 var speedY = 1;
 var gravity = 0.01;
+var mouseDistance = 40;
 
 // create particles
 function initParticles() {
@@ -49,16 +50,12 @@ function createParticle(i) {
 
 function Particle(x, y, vx, vy, size, color, opacity) {
 
-    var originalSize = size;
+    var originalSize = size,
+        maxSize = size * 5;
 
     function reset() {
-        if(mouse) {
-            x = mouse.x;
-            y = mouse.y;
-        } else {
-            x = width * 0.5;
-            y = height * 0.5;
-        }
+        x = width * 0.5;
+        y = height * 0.5;
         opacity = 0.5 + Math.random()*0.5;
         vx = -2+Math.random()*4;
         vy = Math.random()*-3;
@@ -80,7 +77,6 @@ function Particle(x, y, vx, vy, size, color, opacity) {
         y += vy;
 
         // check if particle is near mouse
-        var range = 40
         if(mouse){
             var pos = {
                 x: x,
@@ -88,12 +84,30 @@ function Particle(x, y, vx, vy, size, color, opacity) {
             }
 
             var distance = calculateDistanceBetweenPoints(mouse, pos);
-            if(distance < range){
+            if(distance < mouseDistance && size < maxSize){
                 size = size+1;
-            } else {
-                if(size > originalSize) {
-                    size = size - 1;
+            } else if(size > originalSize) {
+                size = size - 1;
+            }
+
+            if(distance < mouseDistance){
+
+                if(mouse.x > pos.x){
+                    // mouse is to the right of particle
+                    x -= Math.random() * distance;
+                } else {
+                    // mouse is to the left of particle
+                    x += Math.random() * distance;
                 }
+
+                if(mouse.y > pos.y){
+                    // mouse is above particle
+                    y -= Math.random() * distance;
+                } else {
+                    // mouse is below particle
+                    y += Math.random() * distance;
+                }
+
             }
 
         }
