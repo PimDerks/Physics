b68af4a8b92c73941065ef0671957cb9816e7707
Pim Derks <info@pimderks.com>
2015-07-07 10:45:17 +0200

    Added acceleration example.

---
+++ basic-example-acceleration/script.js
@@ -0,0 +1,50 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// height and width of cube
+var widthCube = 20;
+var heightCube = 40;
+
+// position
+var x = 0;
+var y = 0;
+
+// velocity
+var vx = 2;
+var vy = 2;
+
+// acceleration
+var ax = 0.5;
+var ay = 0.5;
+
+// animation loop
+function animate() {
+
+    // clear canvas
+    ctx.clearRect(0, 0, width, height);
+
+    // draw square
+    ctx.fillRect(x,y, widthCube, heightCube);
+
+    // update velocity with acceleration
+    vx += ax;
+    vy += ay;
+    
+    // update position with velocity
+    x += vx;
+    y += vy;
+
+    // raf
+    requestAnimationFrame(animate);
+
+    // if square is out of view reset position to the start
+    if (y > height || x > width) {
+        x = 0;
+        y = 0;
+    }
+}
+
+// start loop
+animate();
